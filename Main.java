81bf0e18d5ac5ebac20f9362dded2ec33fd48cad
vincentzouras <vincentzouras@gmail.com>
2024-08-27 17:05:16 -0400

    Basic functionality

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        String url = "https://www.scrapethissite.com/pages/forms/";
+        Document document = Jsoup.connect(url).get();
+        Elements teams = document.select(".team");
+        for (Element team : teams){
+            String name = team.select(".name").text();
+            System.out.println(name);
+        }
     }
 }
